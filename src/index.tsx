13df4341c14d7abd12c62bd2e4e317f012e68959
GinaIsaia <gina.isaia@ing.com>
2024-04-22 00:12:24 +0200

    added basename for deploy
    Signed-off-by: GinaIsaia <gina.isaia@ing.com>

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter
-    // basename="/portfolio"
+    basename="/portfolio"
     >
       <App />
     </BrowserRouter>
