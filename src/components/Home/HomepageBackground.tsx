21a34b350889cf8dc87618a696efe07833aeebc4
Gina Isaia <gina_isaia@hotmail.com>
2024-05-06 12:48:40 +0200

    added color variables instead of hardcoded
    Signed-off-by: Gina Isaia <gina_isaia@hotmail.com>

--- src/components/Home/HomepageBackground.tsx
+++ src/components/Home/HomepageBackground.tsx
@@ -1,6 +1,6 @@
 import Particles, { initParticlesEngine } from '@tsparticles/react'
 import { useEffect, useState } from 'react'
-import { loadSlim } from '@tsparticles/slim' // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from '@tsparticles/slim'
 
 const HomepageBackground = () => {
   const [init, setInit] = useState(false)
